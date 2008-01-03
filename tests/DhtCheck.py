4655eddb7d21903de0b607d9837151f95faea6bc
davidiw@davidiw-desktop <davidiw@davidiw-desktop>
2008-01-03 16:01:09 -0500

    Minor tweak for DhtCheck to deal with base64 encoding in xmlrpc

--- tests/DhtCheck.py
+++ tests/DhtCheck.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python
-import xmlrpclib, sys
+import xmlrpclib, sys, base64
 
 ip = "127.0.0.1"
 port = "10000"
 server = xmlrpclib.Server("http://" + ip + ":" + port + "/xm.rem")
-print server.localproxy("dht.Dump")
+res = server.localproxy("dht.Dump")
+for l1 in res:
+  for l2 in l1:
+    for l3 in l2:
+      for l4 in l3.iterkeys():
+        if l4 == "key" or l4 == "value":
+          l3[l4] = base64.b64decode(l3[l4].data)
+      print l3
 print server.localproxy("dht.Count")
