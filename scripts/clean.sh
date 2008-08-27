b7d8d9340feff54050181b0521f2048ad5f16bbd
David Wolinsky <davidiw@ufl.edu>
2008-08-27 11:12:57 -0400

    clean.sh will now clean individual stacks as well as top level, minor bug fix for modules that use sscndor, and updated Security dll

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 dir="/usr/local/ipop"
 
-paths="/opt/condor/var
-/var/log
-/tmp
-/var/run
-/var/lib/apt
-/usr/local/ipop/var
-/etc/condor
-/root/.wapi
+paths="opt/condor/var
+var/log
+tmp
+var/run
+var/lib/apt
+usr/local/ipop/var
+etc/condor
+root/.wapi
 "
-for i in $paths; do
-  for j in `find $i`; do
-    rm -f $j/* &> /dev/null
-    rm -f $j/.* &> /dev/null
+
+for k in "/.unionfs/.unionfs" "/.unionfs" "/"; do
+  for i in $paths; do
+    for j in `find $i`; do
+      rm -f $k/$j/* &> /dev/null
+      rm -f $k/$j/.* &> /dev/null
   done
+  rm -f $k/home/griduser/.xison \
+  $k/home/griduser/.xdisabled \
+  $k/var/cache/apt/archives/*deb &> /dev/null
+
+  if [[ k != "/" ]]; then
+    rm -f $k/home/griduser/.Xauthority &> /dev/null
+  fi
 done
 
-rm -f /home/griduser/.xison \
-/home/griduser/.xdisabled \
-/var/cache/apt/archives/*deb
 touch /var/log/dmesg
 touch /var/log/wtmp
